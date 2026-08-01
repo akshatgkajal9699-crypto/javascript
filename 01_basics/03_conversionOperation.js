bad957a9ffd4ff452ccab8796f2e530f6a9a1742
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-01 15:38:06 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
-// let score = null
+let score = null
 // console.log(typeof score);
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
@@ -10,14 +10,29 @@
 let isLoggedIn = 1
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 // 1=> true; 0=> false
 // ""=>false
 // "akshat"=> true
 
 let diffNumber = 44
 let stringNumber = String(diffNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+let str1 = "hello"
+let str2 = "akshat"
+let str3 = str1 +str2
+// console.log(str3);
 
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
