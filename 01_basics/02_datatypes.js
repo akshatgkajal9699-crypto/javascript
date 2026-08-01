0155295b3b2cfba060ca72fdfa8d5836628d6f81
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-01 17:18:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+let name = "akshat"
+// string data type
+let age = 18
+// number data type
+let isLoggedIn = false  
+// boolean data type
+// number=> 2 to the power 53
+// bigInt
+// boolean =>true/false
+// string =>""
+// null=> standolane value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "hitesh" );
+console.log(typeof null);
+console.log(typeof undefined);
+
