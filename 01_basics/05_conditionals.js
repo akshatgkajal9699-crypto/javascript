ffafc37c431549719832602f4483100f04ee6090
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-01 17:07:12 +0000

    conditionals

---
+++ 01_basics/05_conditionals.js
@@ -0,0 +1,17 @@
+let temperature;
+temperature = 30;
+// if(temperature<40){
+//     console.log("its moderate outside")
+// }
+
+if(true){
+    console.log("its very cold outside");
+    
+}
+if(temperature<=20){
+    console.log("its very cold outside");
+    
+}else{
+    console.log("its really hot outside");
+    
+}
