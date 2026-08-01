78a1b1a0a346aa6ef83cf3892607e25754b552dc
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-01 16:49:32 +0000

    example

---
+++ 01_basics/04_example.js
@@ -0,0 +1,6 @@
+let sellingPrice = 354
+let listingPrice = 578
+let discountPercentage = ((listingPrice-sellingPrice)/listingPrice)*100
+console.log(discountPercentage);
+displayDiscountPercentage = Math.round(discountPercentage);
+console.log(displayDiscountPercentage);
