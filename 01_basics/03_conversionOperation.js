1d1abbe9407bc9f14005e9c515ad58a1b686358e
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-01 15:12:19 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = null
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"=> 33
+// "33abch"=> NaN
+// true => 1
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+// 1=> true; 0=> false
+// ""=>false
+// "akshat"=> true
+
+let diffNumber = 44
+let stringNumber = String(diffNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
