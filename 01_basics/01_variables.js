11df8dd1e1587e7570a585864b9e18c5abd5bac1
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-01 14:31:02 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12234
+let accountEmail = "akshat@gmail.com"
+var accountPassword = "123456789"
+accountCity = "Mumbai"
+
+// console.log(accountId);
+accountEmail="akshatgandhi@.com"
+accountPassword="67890"
+accountCity = "pune"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
