f9cfa63b44f08fa0316b98430dbe9aff4a94352d
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-02 07:04:17 +0000

    nums_and_maths

---
+++ 01_basics/08_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score = 400
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 49.567
+// console.log(anotherNumber.toPrecision(2));
+
+const hundereds = 100000
+// console.log(hundereds.toLocaleString);
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.7));
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random() * (max-min+1))+min);
