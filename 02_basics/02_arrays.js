ae75407ba38e66781bd25602aacde00a407a453c
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-02 08:38:40 +0000

    02arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,16 @@
+// const marvel_heros = ["ironman","spiderman","thor"]
+// const dc_heros = ["superman","flash","batman"]
+// // marvel_heros.push(dc_heros)
+// // console.log(marvel_heros);
+// // const allHeros = marvel_heros.concat(dc_heros)
+// // console.log(allHeros);
+// const all_newHeros = [...marvel_heros,...dc_heros]
+// console.log(all_newHeros);
+
+console.log(Array.isArray("akshat"));
+console.log(Array.from("akshat"));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
