5eeb1a02e2c16bd73353f1c720d5caf1e60be9e7
akshatgkajal9699-crypto <akshatg.kajal9699@gmail.com>
2026-08-03 18:42:33 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,30 @@
+// singleton
+// const instaUser = new Object()
+const instaUser = {}
+instaUser.id = "123abc"
+instaUser.name = "jack"
+instaUser.isLoggedIn = false
+console.log(instaUser);
+
+const regularUser = {
+    email : "akshat@gmial.com",
+    fullname : {
+        username:{
+            firstname : "akshat",
+            lastname : "gandhi"
+        }
+    }
+}
+// console.log(regularUser.fullname);
+// merging of objects 
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+// const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+console.log(Object.keys(instaUser));
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser));
+
+console.log(instaUser.hasOwnProperty('isLoggedIn'));
